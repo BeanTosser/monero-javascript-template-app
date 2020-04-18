42ff0b745c90281b6138d6b5f9bb59a7af421062
timetherewere <mikjosjon@gmail.com>
2020-04-18 13:13:38 -0400

    Finished stripping xmr-sample-app down to barebones template app.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 /**
- * Sample browser application which uses a JavaScript library to interact
- * with a Monero daemon using RPC and a Monero wallet using RPC and WASM
- * bindings.
+ * This is a bare-bones monero-javascript web app. Its purpose is to 
+ * provide a basic boilerplate template script on top of which developers
+ * can quickly build their monero-javascript apps. 
+ *
+ * The script uses Monero-javascript's WASM functionality to create an
+ * entirely client-side, offline wallet, and prints some of the wallet's
+ * basic attributes to a web page.
  */
 require("monero-javascript");
 
@@ -11,30 +15,24 @@ runMain();
  * Main thread.
  */
 async function runMain() {
-  let browserMessageSpace = document.getElementById("messageSpace");
-
-  console.log("RUN MAIN");
-  
   // load wasm module on main thread
-  console.log("Loading wasm module on main thread...");
   await MoneroUtils.loadKeysModule();
-  console.log("done loading module");
-  
+
   // create a random keys-only wallet
   let walletKeys = await MoneroWalletKeys.createWalletRandom(MoneroNetworkType.STAGENET, "English");
-  console.log("Keys-only wallet random mnemonic: " + await walletKeys.getMnemonic());
   
-  // print wallet attributes
+  // Create a JSON object to hold select wallet attributes
   let walletInfoJson = {
+	mnemonic: await(walletKeys.getMnemonic()),
 	viewkey: await(walletKeys.getPrivateViewKey()),
 	spendkey: await(walletKeys.getPrivateSpendKey()),
 	address: await(walletKeys.getAddress(0,0))
   };
 
-  let walletAttributes = "Keys-only wallet attributes: " + JSON.stringify(walletInfoJson, undefined, 2);
-  browserMessageSpace = document.getElementById("messageSpace"); 
-  browserMessageSpace.innerHTML = walletAttributes;
-  console.log(walletAttributes);
+  // Convert walletInfoJson object to a string formatted to display
+  // in a readable manner on the HTML page
+  let walletAttributesString = "Keys-only wallet attributes: " + JSON.stringify(walletInfoJson, undefined, "<br/><br/>");
 
-  console.log("EXIT MAIN");
+  // Print the wallet attributes to the page
+  document.getElementById("messageSpace").innerHTML = walletAttributesString;
 }
